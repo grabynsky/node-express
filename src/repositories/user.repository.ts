f7cbc4673df2b206183aa49917a1e25010e01bc1
Grabynsky Andrew <hrabynsky@gmail.com>
2025-03-11 09:13:10 +0200

    typesscript

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,25 @@
+import {IUser, IUserDTO} from "../interfaces/user.interface";
+import {User} from "../modules/user.module";
+
+class UserRepository {
+    public getAll(): Promise<IUser[]> {
+        return User.find()
+    }
+
+    public create(user: IUserDTO): Promise<IUser> {
+        return User.create(user)
+    }
+
+    public getById(userId: string): Promise<IUser>{
+        return User.findById(userId);
+    }
+    public updateById(userId: string, user: IUser): Promise<IUser> {
+         return User.findByIdAndUpdate(userId, user)
+    }
+
+    public deleteById(userId: string){
+   return User.findByIdAndDelete(userId)
+    }
+}
+
+export const userRepository = new UserRepository();
