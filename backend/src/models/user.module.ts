cb8cfa7b7faa0d712a759af8a277a6e30020b6fb
Grabynsky Andrew <hrabynsky@gmail.com>
2025-04-02 21:50:13 +0300

    pizza backend

--- backend/src/models/user.module.ts
+++ backend/src/models/user.module.ts
@@ -20,7 +20,16 @@ const userSchema = new Schema(
         isVerified: { type: Boolean, default: false },
         isDeleted: { type: Boolean, default: false },
     },
-    { timestamps: true, versionKey: false },
+    {
+        timestamps: true,
+        versionKey: false,
+        toJSON: {
+            transform: (doc, ret) => {
+                delete ret.password;
+                return ret;
+            },
+        },
+    },
 );
 
 export const User = model<IUser>("user", userSchema);
